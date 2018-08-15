b9ace2e82ca3dcb43b96c384afb5be0bd427a656
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-08-15 10:13:47 -0400

    Firebase initial configuration added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import * as firebase from "nativescript-plugin-firebase";
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html",
+  selector: "ns-app",
+  templateUrl: "app.component.html"
 })
-
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    firebase
+      .init()
+      .then(() => console.log("Firebase init successfully!"))
+      .catch(error => console.error(`Error: ${error}`));
+  }
+}
